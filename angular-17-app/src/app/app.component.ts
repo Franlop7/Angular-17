682fb9a90646e78396bc9186b776204dd37b15c1
Fran Lopez <franciscolopto@gmail.com>
2024-01-11 21:23:07 +0100

    add styles in line

--- angular-17-app/src/app/app.component.ts
+++ angular-17-app/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, UserComponent],
+  template: `<h1>Hola {{ title }}</h1> 
+    <app-user />`,
+  styles: ``, 
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-17-app';
