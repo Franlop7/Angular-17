66fd7f4c62b9d6c9501383420a78543281131ce8
Fran Lopez <franciscolopto@gmail.com>
2024-01-16 21:37:47 +0100

    add import gamescomponent

--- angular-17-app/src/app/user/user.component.ts
+++ angular-17-app/src/app/user/user.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { GamesComponent } from '../games/games.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [GamesComponent],
 })
 export class UserComponent {
   username = 'Fran';
-  isLoggedIn = true;
+  isLoggedIn = false;
+  favGame = '';
+
+  getFavorite(gameName: string) {
+    this.favGame = gameName;
+  }
+
+  greet() {
+    alert('Siuuuu!!');
+  }
 }
