3a94f830b7b8bfe60612b9ed021f34cd132201ae
Fran Lopez <franciscolopto@gmail.com>
2024-01-22 13:31:13 +0100

    change styles in line

--- angular-17-app/src/app/app.component.ts
+++ angular-17-app/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { CommentsComponent } from "./comments/comments.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent],
-  // template: `<h1>Hola {{ title }}</h1> 
-  //   <app-user />`,
-  styles: ``, 
-  templateUrl: './app.component.html',
-  // styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    // template: `<h1>Hola {{ title }}</h1>
+    //   <app-user />`,
+    styles: ``,
+    templateUrl: './app.component.html',
+    // styleUrl: './app.component.css',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, UserComponent, CommentsComponent]
 })
 export class AppComponent {
   title = 'angular-17-app';
